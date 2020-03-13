ca7fc031464d1bc65ca2840e63f05e757e8936b2
Romeo60 <48881861+Romeo60@users.noreply.github.com>
2020-03-13 09:44:00 +0200

    Update index.php

--- index.php
+++ index.php
@@ -1,26 +1,44 @@
-<?php
-ini_set('error_reporting', E_ALL);
-ini_set('display_errors', 1);
-ini_set('display_startup_errors',1);
-ini_set('log_errors',1);
-ini_set('log_errors_max_len',1024);
-ini_set('ignore_repeated_errors',0);
-ini_set('ignore_repeated_source',0);
-ini_set('report_memleaks',1);
-ini_set('track_errors',1);
-ini_set('error_log',"php_error_log.txt" );
+<center>
+	<div align="center" class="animated pulse slow" style="color: orange;"><h1><?=$GLOBALS["title"]?></h1></div>
+<h1>Get in touch with us</h1>
+<p>Go ahead and say hi, we sure would love hearing from you and we will make sure to reply as soon as possible</p>
+<div class="col-xl-8 offset-xl-2 py-5">
 
-// place website in development mode
-$_GET["dev"] = true;
+	<form id="contact-form" method="post" role="form">
+	<input class="form-control" type="text" name="name" placeholder="Full Name*"></br>
+	<input class="form-control" type="email" name="email" placeholder="Email Adress*"></br>
+	<input class="form-control" type="text" name="subject" placeholder="Subject"></br>
+	<textarea class="form-control"  id="comment" name="message" placeholder="Message..." rows=5; cols="5"></textarea> </br>
+	<button class="btn btn-success navbar-btn"  name="submit"type="submit">Send</button>
+	<?error();?>
+</form>
+<p>Or</p>
+<a href="callto:0658522045">Call Contact At Company</a>
+</div>
+</center>
 
-include_once("Router.php");// removed all the includes folder tags, because uploaded the files to the main depository
-include_once("Template.php");
+<?
+success();
+if (isset($_POST["submit"])) {
+	$to_mail = "romeo607080@gmail.com";
+	$subject =$_POST["subject"];
+ 	$message = $_POST["message"];
+ 	$header = "From: ".$_POST["email"];
+}
+ 
 
-$GLOBALS["SiteName"] = "Company";
-$GLOBALS["SiteEmail"] = "admin@company.co.za";
-
-$router = new Router("home", "webcontent");
-
-/*$router->set("home","HomeController",array("generic_name"=>null,"table_name"=>null),array());*/
-
-$router->run();
+ if(mail($to_mail, $subject, $message, $header)){
+ 	echo ("svn_fs_dir_entries(fsroot, path)");
+ }else{
+ 	echo ("error");
+ }
+ 
+?>
+<script type="text/javascript">
+	function error(){
+		window.alert("Something went wrong, try again or callus");
+	}
+	function success(){
+		window.alert("Thank you for contacting us, you'll hear from us soon");
+	}
+</script>
